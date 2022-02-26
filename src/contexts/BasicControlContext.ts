ee79744bca3776731600fa441dedcda5dfe11a0a
Akia Vongdara <vongdarakia@gmail.com>
2022-02-25 22:15:23 -0800

    Remove unused code

--- src/contexts/BasicControlContext.ts
+++
@@ -1,7 +0,0 @@
-import { createSubscriberContext } from "../react-subscribe-context/createSubscriberContext";
-
-const defaultState = {
-    counter: 0,
-};
-
-export const BasicControlContext = createSubscriberContext({ defaultState });
