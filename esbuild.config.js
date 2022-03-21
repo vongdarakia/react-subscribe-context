0e322d66c7ae919be63f7a51dfbc24dd7b8db7b8
Akia Vongdara <vongdarakia@gmail.com>
2022-03-21 16:14:36 -0700

    Add sourcemap to esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,5 +5,7 @@ esbuild
         entryPoints: ["react-subscribe-context/index.ts"],
         bundle: true,
         outfile: "dist/index.js",
+        sourcemap: true,
+        minify: true,
     })
     .catch(() => process.exit(1));
